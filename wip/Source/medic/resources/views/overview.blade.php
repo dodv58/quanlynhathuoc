f905177548f239587d694306885e03bbacc3fc53
giangnpt94 <giangnpt94@gmail.com>
2017-05-10 21:22:36 +0700

    Fix bug layout of overview page

--- wip/Source/medic/resources/views/overview.blade.php
+++ wip/Source/medic/resources/views/overview.blade.php
@@ -173,7 +173,7 @@
 @endsection
 
 @section('jsLib')
-    <script src="{{ asset('vendors/bower_components/bootstrap-progressbar/bootstrap-progressbar.js') }}"></script>
     <script src="{{ asset('js/custom.min.js') }}"></script>
+    <script src="{{ asset('vendors/bower_components/bootstrap-progressbar/bootstrap-progressbar.js') }}"></script>
 
 @endsection
